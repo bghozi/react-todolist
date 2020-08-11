67b485732f9395b6d1ae205744a061af0b80b10b
bghozi <50072379+bghozi@users.noreply.github.com>
2020-08-11 16:25:11 +0700

    membuat todos dan todolist component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,40 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Todo from "./Todo";
 
-const Todos = () => {
-  const todos = [
-    {
-      text: "Belajar React!"
-    },
-    {
-      text: "React"
-    },
-    {
-      text: "Belajar!"
-    },
-    {
-      text: "Belajar React"
-    },
-    {
-      text: "Belajar"
-    },
-    {
-      text: "Belajar React!"
-    },
-    {
-      text: "React"
-    },
-    {
-      text: "Belajar!"
-    },
-    {
-      text: "Belajar React"
-    },
-    {
-      text: "Belajar"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map((todo) => {
@@ -42,6 +11,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
