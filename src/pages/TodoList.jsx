d3748d4add4d00bd59596cd02ccd8c9169699a94
bghozi <50072379+bghozi@users.noreply.github.com>
2020-08-11 20:24:48 +0700

    delete function dan placeholder

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -14,9 +14,12 @@ const TodoList = () => {
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value, isCompleted: false }];
-
-    setTodos(addedTodo);
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
+      setTodos(addedTodo);
+    } else {
+      alert("Omly 10 Todo");
+    }
   };
   const completeTodo = (index) => {
     const addedTodo = [...todos];
@@ -24,13 +27,18 @@ const TodoList = () => {
 
     setTodos(addedTodo);
   };
+  const clearTodos = () => setTodos([]);
 
   const showAddToggle = () => setShowAdd(!showAdd);
 
   console.log("todos", todos);
   return (
     <Paper>
-      <Header showAddToggle={showAddToggle} />
+      <Header
+        showAddToggle={showAddToggle}
+        showAdd={showAdd}
+        clearTodos={clearTodos}
+      />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
